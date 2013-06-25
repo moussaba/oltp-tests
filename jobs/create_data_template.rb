e1e2dada6714f460a1332cae8fd0d1f4e2167c7e
Moussa Ba <moussaba@micron.com>
2013-06-25 16:05:24 -0700

    SWR_DRIVE fix for create_data

--- jobs/create_data_template.rb
+++ jobs/create_data_template.rb
@@ -3,51 +3,111 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','scripts','benchm
 
 class JobCreateDataTemplate < SwrJob
 
-  def setup(verbose)
+  def initialize(configfiles)
+    super(configfiles)
+    case ENV["SWR_DB_TYPE"]
+      when "mysql"
+        @datadir = @config["mysql"]["named_args"]["datadir"]
+        @logdir = @config["mysql"]["named_args"]["innodb_log_group_home_dir"]
+
+      when "mongodb"
+        @datadir = @config["mongodb"]["named_args"]["dbpath"]
+        @logdir = ""
+      when "cassandra"
+        @datadir = @config["cassandra"]["conf"]["data_file_directories"]
+        @logdir = ""
+
+    end
+  
     puts "------kill off stuff----------"
+
+  end
+
+  def setup(verbose)
+    
+    shell = SwrShell.new
+    drive = ENV["SWR_DRIVE"]
     kill_all(verbose,true)
     puts "------umount----------"
     @fileutils.sync verbose
-    @drives.umount(verbose,true)
-    puts "------partition----------"
-    @drives.partition(verbose)
-    puts "------format-------------"
-    @drives.format(verbose)
-    puts "------mount--------------"
-    @drives.mount(verbose)
+    shell.execute("sudo umount #{drive}",true,status_can_be_nonzero=true)
+    #@drives.umount(verbose,true)
+    #puts "------partition----------"
+    #@drives.partition(verbose)
+    #puts "------format-------------"
+    #@drives.format(verbose)
+    #@drives.mount(verbose)
     puts "------mkdirs-------------"
-    if not file_exists?(@config["mysql"]["named_args"]["datadir"])
-      @fileutils.su_mkdir_p @config["mysql"]["named_args"]["datadir"], verbose
+    puts "Data dir" + @datadir
+    if not file_exists?(@datadir)
+      @fileutils.su_mkdir_p @datadir, verbose
     end
-    if not file_exists?(@config["mysql"]["named_args"]["innodb_log_group_home_dir"])
-      @fileutils.su_mkdir_p @config["mysql"]["named_args"]["innodb_log_group_home_dir"], verbose
+    puts "------mount--------------"
+    @fileutils.su_mount drive + " " + @datadir
+    if not file_exists?(@logdir) 
+        if @logdir != ''    	 
+            @fileutils.su_mkdir_p @logdir, verbose
+        end
     end
+    dirall = @datadir+"/*"
+    puts "-----Removing datadir----"
+    cmd = "sudo rm -rf #{dirall}"
+    shell.execute(cmd,verbose=true,status_can_be_nonzero=true)
   end
 
   def tar_data(verbose)
     @fileutils.su_mkdir_p @config["tar_data"], verbose
-    @fileutils.su_du_sh "#{@config["mysql"]["named_args"]["datadir"]}", verbose
-    @fileutils.su_tar " -C #{@config["mysql"]["named_args"]["datadir"]} "," -czf #{File.join(@config["tar_data"],"data.tar.gz")}", ".", verbose
-    if @config["mysql"]["named_args"]["datadir"] == @config["mysql"]["named_args"]["innodb_log_group_home_dir"]
+    @fileutils.su_du_sh "#{@datadir}", verbose
+    @fileutils.su_tar " -C #{@datadir} "," -czf #{File.join(@config["tar_data"],"data.tar.gz")}", ".", verbose
+    if @datadir == @logdir
       #log and data dirs are the same so creating empty log.tar.gz
       @fileutils.su_mkdir_p "empty", verbose
       @fileutils.su_tar " -C empty "," -czf #{File.join(@config["tar_data"],"log.tar.gz")}",".", verbose
       @fileutils.su_rm_rf "empty", verbose
-    else
-      @fileutils.su_tar " -C #{@config["mysql"]["named_args"]["innodb_log_group_home_dir"]} "," -czf #{File.join(@config["tar_data"],"log.tar.gz")}",".", verbose
+    elsif @logdir != ''
+      @fileutils.su_tar " -C #{@logdir} "," -czf #{File.join(@config["tar_data"],"log.tar.gz")}",".", verbose
     end
-    shell = SwrShell.new
-    cmd = "cd #{@config["tar_data"]}; du -h; sudo touch md5sums.txt; sudo chmod a+rw md5sums.txt; sudo md5sum data.tar.gz >> md5sums.txt; sudo md5sum log.tar.gz >> md5sums.txt;"
-    shell.execute(cmd,verbose)
+    #shell = SwrShell.new
+    #cmd = "cd #{@config["tar_data"]}; du -h; sudo touch md5sums.txt; sudo chmod a+rw md5sums.txt; sudo md5sum data.tar.gz >> md5sums.txt; sudo md5sum log.tar.gz >> md5sums.txt;"
+    #shell.execute(cmd,verbose)
     @fileutils.sync verbose
   end
 
   def teardown(verbose)
+    shell = SwrShell.new
     puts "------umount----------"
-    @drives.umount(verbose)
+    #@drives.umount(verbose)
+    drive = ENV["SWR_DRIVE"]
+    shell.execute("sudo umount #{drive}",true,status_can_be_nonzero=true)
+  end
+
+  def start_db(verbose)
+    case ENV["SWR_DB_TYPE"]
+      when "mysql"
+        start_mysql(nil,verbose)
+      when "mongodb"
+        start_mongodb(verbose)
+      when "cassandra"
+        start_cassandra(verbose)
+    end
+  end
+
+  def shutdown_db(verbose)
+    case ENV["SWR_DB_TYPE"]
+      when "mysql"
+        shutdown_mysql(verbose)
+      when "mongodb"
+        shutdown_mongodb(verbose)
+      when "cassandra"
+        shutdown_cassandra(verbose)
+    end
   end
 
   def start_mysql(buffer_pool_size,verbose)
+    puts "------initializing mysql db------------"
+    shell = SwrShell.new
+    cmd = "sudo mysql_install_db --datadir=#{@config["mysql"]["named_args"]["datadir"]}"
+    shell.execute(cmd,verbose)
     puts "------starting mysqld with buffer_pool_size=#{buffer_pool_size}------------"
     @mysqld = SwrMysqld.new(@config["mysql"])
     if buffer_pool_size != nil
@@ -55,6 +115,22 @@ class JobCreateDataTemplate < SwrJob
     end
     @mysqld.start(verbose)
     sleep 60
+  end
+
+  def start_mongodb(verbose)
+    puts "------initializing mongodb------------"
+    shell = SwrShell.new
+    @mongodb = SwrMongoDB.new(@config["mongodb"])
+    @mongodb.start(verbose=true)
+
+  end
+
+  def start_cassandra(verbose)
+    puts "------initializing mongodb------------"
+    shell = SwrShell.new
+    @mongodb = SwrCassandra.new(@config["cassandra"])
+    @mongodb.start(verbose=true)
+
   end
 
   def shutdown_mysql(verbose)
@@ -67,18 +143,24 @@ class JobCreateDataTemplate < SwrJob
     puts "status[" + status.to_s + "]"
   end
 
+  def shutdown_mongodb(verbose)
+    @mongodb.shutdown(verbose)
+  end
+
+  def shutdown_cassandra(verbose)
+    @cassandra.shutdown(verbose)
+  end
+
   def doit(verbose)
-    puts "------initializing mysql db------------"
-    shell = SwrShell.new
-    cmd = "sudo mysql_install_db --datadir=#{@config["mysql"]["named_args"]["datadir"]}"
-    shell.execute(cmd,verbose)
-    start_mysql(nil,verbose)
+    start_db(verbose)
+    args_order=["extra_args","short_args","named_args"]
     puts "------starting benchmark------------"
     @benchmark = @benchmarks.get_new(@config["benchmark"],@mysqld)
     @benchmark.make(verbose)
     @benchmark.create(verbose)
     @benchmark.load(verbose)
-    shutdown_mysql(verbose)
+    shutdown_db(verbose)
+    shell = SwrShell.new
   end
 
   def get_jenkins_parameters
