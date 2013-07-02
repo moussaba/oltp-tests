cb12a1dacc0dbe89d6a98bb4cc13307855958b25
Jared Hulbert <jehulber@micron.com>
2013-07-02 04:40:30 -0700

    madness

--- jobs/run_oltp.rb
+++ jobs/run_oltp.rb
@@ -1,10 +1,24 @@
 require File.expand_path(File.join(File.dirname(__FILE__),'..','scripts','common','swr_common.rb'))
-require File.expand_path(File.join(File.dirname(__FILE__),'..','scripts','benchmarks','swr_sysbench.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','scripts','benchmarks','swr_benchmarks.rb'))
 
-class JobRunBenchmark < SwrJob
+class JobRunOltp < SwrJob
+
+  def initialize(args)
+    super(args)
+
+    @datadir = @config["mysql"]["named_args"]["datadir"]
+    @logdir = @config["mysql"]["named_args"]["innodb_log_group_home_dir"]
+    @drive = @config['env']["SWR_DRIVE"]
+    @config["benchmark"]["run_time"] = @config['env']["SWR_RUNTIME"]
+    @benchmarks = SwrBenchmarks.new
+    @benchmarks.set_parameters(@config)
+    if @config['env']["SWR_TEST"] == "sysbench-readonly"
+      @configs["benchmark"]["named_args"]["oltp-read-only"]="on"
+    end
+  end
 
   def copy_data?
-    copydata = ENV["SWR_COPY_DATA"]
+    copydata = @config['env']["SWR_COPY_DATA"]
     if copydata != nil
       if copydata == "false"
         return false
@@ -18,7 +32,7 @@ class JobRunBenchmark < SwrJob
   end
 
   def pre_condition?
-    pre_condition = ENV["SWR_PRECONDITION"]
+    pre_condition = @config['env']["SWR_PRECONDITION"]
     if pre_condition != nil
       if pre_condition == "false"
         return false
@@ -27,97 +41,73 @@ class JobRunBenchmark < SwrJob
     return true
   end
 
-
   def setup(verbose)
-    drive = ENV["SWR_DRIVE"]
     shell = SwrShell.new
-    puts "------kill off stuff-----"
-    kill_all(verbose,true)
     puts "------umount-------------"
     @fileutils.sync verbose
-    shell.execute("sudo umount #{drive}",true,status_can_be_nonzero=true)
-    cmd = "sudo umount " + config["mysql"]["named_args"]["datadir"]
-    shell.execute(cmd,true,status_can_be_nonzero=true)
+    cmd = "sudo umount " + @drive
+    shell.execute(cmd,true,true)
+    cmd = "sudo umount " + @datadir
+    shell.execute(cmd,true,true)
     if pre_condition?
       puts "-----Pre Conditionning Drive with Sequential Fill -----------"
-      cmd = "sudo dd if=/dev/zero of=#{drive} bs=1M count=10"
-      shell.execute(cmd,true,status_can_be_nonzero=true)
+      cmd = "sudo dd if=/dev/zero of=#{@drive} bs=1M"
+      shell.execute(cmd,true,true)
     end
 
     if copy_data? || pre_condition?
       puts "------clean up-----------"
-      @fileutils.su_rm_rf @config["mysql"]["named_args"]["datadir"], verbose
-      @fileutils.su_rm_rf @config["mysql"]["named_args"]["innodb_log_group_home_dir"], verbose
+      @fileutils.su_rm_rf @datadir, verbose
+      @fileutils.su_rm_rf @logdir, verbose
       puts "-----Making File System---------"
-      shell.execute("sudo mkfs.ext4 -F #{drive}",true)
+      shell.execute("sudo mkfs.ext4 -F #{@drive}",true)
       puts "------mkdirs-------------"
-      if not file_exists?(@config["mysql"]["named_args"]["datadir"])
-        @fileutils.su_mkdir_p @config["mysql"]["named_args"]["datadir"], verbose
+      if not file_exists?(@datadir)
+        @fileutils.su_mkdir_p @datadir, verbose
       end
-      if not file_exists?(@config["mysql"]["named_args"]["innodb_log_group_home_dir"])
-        @fileutils.su_mkdir_p @config["mysql"]["named_args"]["innodb_log_group_home_dir"], verbose
+      if not file_exists?(@logdir)
+        @fileutils.su_mkdir_p @logdir, verbose
       end
-      puts "------mount--------------"
-      @fileutils.su_mount drive + " -o nobarrier " + config["mysql"]["named_args"]["datadir"]
-      puts "------cp data-------------"
-      copy_data
-      #cmd = "sudo tar -C " + @config["mysql"]["named_args"]["datadir"] "/backup_data/" \
-      #                     + ENV["SWR_TEST"] + "/data.tar.gz"
-      #shell.execute(cmd,true)
-      #cmd = "sudo tar -C " + @config["mysql"]["named_args"]["datadir"] "/backup_data/" + ENV["SWR_TEST"] + "/log.tar.gz"
-      #shell.execute(cmd,true)
-    else
-     puts "------mount--------------"
-     @fileutils.su_mount drive + " -o nobarrier " + config["mysql"]["named_args"]["datadir"]
     end
-
-    # Setup benchmark time
+    puts "------mount--------------"
+    @fileutils.su_mount @drive + " -o nobarrier " + @datadir
+    if copy_data? || pre_condition?
+      puts "------cp data-------------"
+      copy_data(verbose)
+    end
   end
 
-
-  def copy_data
-      shell = SwrShell.new
-      dirloc = "/backup_data/mysql/"
-      case ENV["SWR_TEST"]
-        when "sysbench"
-          dirloc = File.join(dirloc,"sysbench","10M_ROWS_100_TABLES")
-        when "sysbench-readonly"
-          dirloc = File.join(dirloc,"sysbench","10M_ROWS_100_TABLES")
-        when "tpcc"
-          dirloc = File.join(dirloc,"tpcc","2500")
-      end
-      filename = dirloc + "/data.tar.gz"
-      datadir = @config["mysql"]["named_args"]["datadir"]
-
-      if File.file?(filename)
-          cmd = "sudo tar -C " + datadir + " -xzf #{filename}"
-          shell.execute(cmd,true)
-          cmd = "sudo tar -C " + datadir + " -xzf #{dirloc}" + "/log.tar.gz"
-          shell.execute(cmd,true)
-      else
-          cmd = "sudo cp -R " + dirloc + "/* " + datadir
-          shell.execute(cmd,true)
-      end
-
+  def do_untar(dstdir,srcdir,filename,verbose)
+    if file_exists?(File.join(srcdir,filename+"tar"))
+      @fileutils.su_tar " -C #{dstdir} ", " -xf #{src}", ".", verbose
+    elsif file_exists?(File.join(srcdir,filename+"tar.gz"))
+      @fileutils.su_tar " -C #{dstdir} ", " -xzf #{src}", ".", verbose
+    elsif file_exists?(File.join(srcdir,filename+"tar.bz2"))
+      @fileutils.su_tar " -C #{dstdir} ", " -xjf #{src}", ".", verbose
+    else
+      raise "not found trolls"
+    end
   end
 
+  def copy_data(verbose)
+    do_untar(@datadir,@config['tar_data'],"data",verbose)
+    do_untar(@logdir,@config['tar_data'],"log",verbose)
+  end
 
   def teardown(verbose)
-    puts "------turn off caching solution-------------"
-    @solution.stop(verbose)
     puts "------umount----------"
-    @drives.umount(verbose,true)
+    shell = SwrShell.new
+    cmd = "sudo umount " + @drive
+    shell.execute(cmd,true,true)
   end
 
-  def start_mysql(buffer_pool_size,verbose)
-    puts "------starting mysqld with buffer_pool_size=#{buffer_pool_size}------------"
-    @mysqld = SwrMysqld.new(@config["mysql"])
-    #@mysqld.args["named_args"]["innodb_buffer_pool_size"] = buffer_pool_size
-    @mysqld.start(verbose)
+  def start_db(verbose)
+    puts "------starting mysqld ------------"
+    @db = SwrMysqld.new(@config["mysql"])
+    @db.start(verbose)
     sleep 60
-    puts "------starting benchmark------------"
-    @config["benchmark"]["run_time"] = ENV["SWR_RUNTIME"]
-    @benchmark = @benchmarks.get_new(@config["benchmark"],@mysqld)
+    puts "------setting up benchmark------------"
+    @benchmark = @benchmarks.get_new(@config["benchmark"],@db)
     @benchmark.set_benchmark_run_time
     puts "------making benchmark------------"
     @benchmark.make(verbose)
@@ -125,11 +115,11 @@ class JobRunBenchmark < SwrJob
     @benchmark.warmup(verbose)
   end
 
-  def shutdown_mysql(verbose)
-    @mysqld.innodb_stats(verbose)
-    @mysqld.shutdown(verbose)
+  def shutdown_db(verbose)
+    @db.innodb_stats(verbose)
+    @db.shutdown(verbose)
     puts "-mysql had this to say ----------------"
-    stdout, stderr, status, cmd = @mysqld.wait
+    stdout, stderr, status, cmd = @db.wait
     puts "cmd[#{cmd}]"
     puts "stdout[#{stdout.chomp}]"
     puts "stderr[#{stderr.chomp}]"
@@ -137,10 +127,9 @@ class JobRunBenchmark < SwrJob
   end
 
   def doit(verbose)
-      buffer_pool_size = @config["mysql"]["innodb_buffer_pool_size"]
-      start_mysql(buffer_pool_size,verbose)
-      @benchmark.start(verbose)
-      shutdown_mysql(verbose)
+    start_db(verbose)
+    @benchmark.start(verbose)
+    shutdown_db(verbose)
   end
 
 end
@@ -153,22 +142,18 @@ configs << "config/mysql/mysql_percona.yaml"
 configs << "config/scenario/24G_1trial_1800.yaml"
 
 case ENV["SWR_TEST"]
-    when "sysbench"
-	configs << "config/benchmark/sysbench_10M_interval.yaml"
-    when "sysbench-readonly"
-	configs << "config/benchmark/sysbench_10M_readonly_interval.yaml"
-    when "tpcc"
-	configs << "config/benchmark/tpcc_2500.yaml"
+  when "sysbench"
+    configs << "config/benchmark/sysbench_10M_100_TABLES_interval.yaml"
+  when "sysbench-readonly"
+    configs << "config/benchmark/sysbench_10M_100_TABLES_interval.yaml"
+  when "tpcc"
+    configs << "config/benchmark/tpcc_2500.yaml"
 end
-#if ENV["SWR_TEST"] == "sysbench"
-#else
-#  configs << "config/benchmark/tpcc_2500.yaml"
-#end
 
-job = JobRunBenchmark.new(configs)
+job = JobRunOltp.new(configs)
 
 job.setup(verbose)
 job.doit(verbose)
-#job.teardown(verbose)
+job.teardown(verbose)
 
 j.print
