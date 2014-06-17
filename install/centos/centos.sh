72ec40717d0f3e6d3659620662ab241b7da596d7
jenkins <jenkins@m1-superbox1.(none)>
2014-06-17 08:28:20 -0700

    Added updated Rubygems

--- install/centos/centos.sh
+++ install/centos/centos.sh
@@ -3,12 +3,17 @@
 #Our proxy
 #export http_proxy=http://proxy.micron.com:8080
 #echo "proxy=http://proxy.micron.com:8080" >> /etc/yum.conf
+export http_proxy=http://proxy.micron.com:8080
+export https_proxy=http://proxy.micron.com:8080
 
 #Install
 yum -y install ruby
 yum -y install rubygems
+cd rubygems-2.0.3
+ruby setup.rb
 gem install open4
 gem install net-ssh
+cd ..
 
 sudo yum install -y gcc ruby-devel libxml2 libxml2-devel libxslt libxslt-devel
 gem install jenkins_api_client
