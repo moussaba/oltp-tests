0c57093178a5133de9db5d9d2b2bee6f21583279
Moussa Ba <moussaba@micron.com>
2014-08-13 17:37:33 -0700

    Test run sysbench_storm

--- vmworld/sysbench_storm.py
+++ vmworld/sysbench_storm.py
@@ -9,7 +9,7 @@ from TCommand import TCommand
 
 
 
-def __run_sysbench(self):
+def __run_sysbench():
         cmd = "ruby jobs/benchmark_oltp_vmworld.rb config/mysql/mysql_percona.yaml config/benchmark/sysbench_10M_ROWS_100_TABLES.yaml config/scenario/24G_1800s.yaml"
         ssh = SSHClient("10.102.41.10","jenkins","123456")
         ssh.execute_command(cmd)
