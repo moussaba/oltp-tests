8ebe82c6fa0e0dd19564e3fed6e57241c25151c2
root <root@localhost.localdomain>
2015-03-12 18:36:35 -0400

    Cleanup script and bring up to date

--- install/centos/centos.sh
+++ install/centos/centos.sh
@@ -1,4 +1,6 @@
 #tested with Centos6.3 run as root
+#tested with Centos7.0 run as root
+#assumes you added proxy=http://proxy.micron.com:8080 to /etc/yum.conf
 
 #Our proxy
 #export http_proxy=http://proxy.micron.com:8080
@@ -9,16 +11,17 @@ export https_proxy=http://proxy.micron.com:8080
 #Install
 yum -y install ruby
 yum -y install rubygems
-cd rubygems-2.0.3
-ruby setup.rb
 gem install open4
 gem install net-ssh
 cd ..
 
 sudo yum install -y gcc ruby-devel libxml2 libxml2-devel libxslt libxslt-devel
+gem install json_pure
 gem install jenkins_api_client
+wget -O /tmp http://pkg.jenkins-ci.org/redhat/jenkins-1.602-1.1.noarch.rpm
+yum install -y /tmp/jenkins-1.602-1.1.noarch.rpm
 
-#wget -r -l 1 -nd -A rpm -R "*debuginfo*" http://www.percona.com/redir/downloads/Percona-Server-5.5/Percona-Server-5.5.30-30.2/RPM/rhel6/x86_64/
+
 yum -y remove mysql mysql-devel mysql-libs
 
 yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
