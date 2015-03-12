1052399eaa0184854894466962fb91e1978a0629
Moussa Ba <moussaba@micron.com>
2015-03-12 14:08:12 -0700

    Updated centos installation script

--- install/centos/centos.sh
+++ install/centos/centos.sh
@@ -18,15 +18,16 @@ cd ..
 sudo yum install -y gcc ruby-devel libxml2 libxml2-devel libxslt libxslt-devel
 gem install jenkins_api_client
 
-wget -r -l 1 -nd -A rpm -R "*debuginfo*" http://www.percona.com/redir/downloads/Percona-Server-5.5/Percona-Server-5.5.30-30.2/RPM/rhel6/x86_64/
+#wget -r -l 1 -nd -A rpm -R "*debuginfo*" http://www.percona.com/redir/downloads/Percona-Server-5.5/Percona-Server-5.5.30-30.2/RPM/rhel6/x86_64/
 yum -y remove mysql mysql-devel mysql-libs
 
-rpm -ivh Percona-Server-shared-55-*.rpm
-rpm -ivh Percona-Server-client-55-*.rpm
-rpm -ivh Percona-Server-server-55-*.rpm
-rpm -ivh Percona-Server-devel-55-*.rpm
+yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-3/percona-release-0.1-3.noarch.rpm
 
-rm -f *.rpm
+yum install -y Percona-Server-shared-55
+yum install -y Percona-Server-client-55
+yum install -y Percona-Server-server-55
+yum install -y Percona-Server-devel-55
+
 
 chkconfig mysql off
 service mysql stop
